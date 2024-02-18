47fb4ca512319ed64e57b653981d7fbd5ef92d05
Brandi Burnell <brandiburnell6@gmail.com>
2024-02-17 17:45:36 -0700

    pass tests

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 23, 'M', 'brown', 'slippery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 23, 'M', 'brown', 'slippery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 23, 'M', 'brown', 'slippery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 23, 'M', 'brown', 'slippery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 23, 'M', 'brown', 'slippery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 23, 'M', 'brown', 'slippery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 23, 'M', 'brown', 'slippery', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (NULL, 23, 'M', 'brown', 'slippery', true);
